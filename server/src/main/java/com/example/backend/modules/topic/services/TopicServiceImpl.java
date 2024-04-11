4230b53003ff62465efef84dcaeaff51fc9eb68e
dong1408 <dongden14082002@gmail.com>
2024-04-11 22:51:02 +0700

    update code

--- server/src/main/java/com/example/backend/modules/topic/services/TopicServiceImpl.java
+++ server/src/main/java/com/example/backend/modules/topic/services/TopicServiceImpl.java
@@ -46,6 +46,31 @@ public class TopicServiceImpl implements TopicService{
         this.fileStorageService = fileStorageService;
     }
 
+    @Override
+    @Transactional
+    public ResponseSuccess<TopicVm> createTopic(CreateTopicDTO dto) throws IOException {
+
+        var topic = topicRepository.findBySlug(dto.getSlug());
+        if(topic.isPresent()){
+            throw new TopicSlugUsedException(TopicConstants.TOPIC_SLUG_USED);
+        }
+        String thumbnailUrl = fileStorageService.uploadFile(dto.getThumbnail());
+
+        Topic newTopic = Topic.builder()
+                .title(dto.getTitle())
+                .createdAt(new Date())
+                .updatedAt(new Date())
+                .thumbnail(thumbnailUrl)
+                .slug(dto.getSlug())
+                .build();
+
+        var save = topicRepository.save(newTopic);
+
+        var topicVm = Utilities.getTopicVm(save);
+
+        return new ResponseSuccess<>(TopicConstants.CREATE_TOPIC,topicVm);
+    }
+
 
     @Override
     public ResponseSuccess<TopicVm> editTopic(EditTopicDTO dto) throws IOException {
@@ -83,33 +108,6 @@ public class TopicServiceImpl implements TopicService{
         return new ResponseSuccess<>(TopicConstants.EDIT_TOPIC,result);
 
     }
-
-    @Override
-    @Transactional
-    public ResponseSuccess<TopicVm> createTopic(CreateTopicDTO dto) throws IOException {
-
-        var topic = topicRepository.findBySlug(dto.getSlug());
-        if(topic.isPresent()){
-            throw new TopicSlugUsedException(TopicConstants.TOPIC_SLUG_USED);
-        }
-        String thumbnailUrl = fileStorageService.uploadFile(dto.getThumbnail());
-
-        Topic newTopic = Topic.builder()
-                .title(dto.getTitle())
-                .createdAt(new Date())
-                .updatedAt(new Date())
-                .thumbnail(thumbnailUrl)
-                .slug(dto.getSlug())
-                .build();
-
-        var save = topicRepository.save(newTopic);
-
-        var topicVm = Utilities.getTopicVm(save);
-
-        return new ResponseSuccess<>(TopicConstants.CREATE_TOPIC,topicVm);
-    }
-
-
     @Override
     public ResponseSuccess<Boolean> deleteTopic(int topicId) {
         var topic = topicRepository.findById(topicId);
